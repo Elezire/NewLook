2ae1898326bd778680ad811f2422a5da6c5ed776
imran-elezire <32159477+imran-elezire@users.noreply.github.com>
2018-02-25 22:07:38 +0530

    newone

---
+++ et/header.php
@@ -0,0 +1,246 @@
+<div class="header header-over large"  id="mainNav">
+			<div class="container">
+				<div class="row">
+					<div class="navbar navbar-default mynav navbar-fixed-top">
+					<div class="col-md-3 col-sm-6 col-xs-5">
+						<!-- Start Logo Image -->
+						<a href="index.php" class="logo-image">
+							<img src="img/logos/logo.png" alt="logo">
+						</a>
+						<!-- End of Logo Image -->
+					</div>
+					<div class="col-md-9 col-sm-6 col-xs-7">
+						<!-- Start Menu -->
+						<nav class="right helper  ">
+							<ul class="menu sf-menu js-menu">
+								<li>
+									<a href="index.php">Home</a>									
+								</li>
+								<li>
+									<a href="#">About Us</a>									
+								</li>
+								<li>
+									<a href="#">Services</a>
+								</li>
+								<li>
+									<a href="#">Products</a>
+								</li>
+								<li>
+									<a href="#">Carrier</a>
+								</li>
+								<li style="margin-left:60px;">
+									EDC 
+								</li>
+								<li style=" margin-right:50px;">
+								<a href="#" class="button stroke small blue">Sign In</a>
+								</li>
+								<li style="margin-top:0px;">
+								<a href="#" class="js-menu-side  menu-side-trigger-right" data-position="right"><i class="fa fa-bars fa-2x"></i></a>
+								</li>								
+							</ul>
+						</nav>
+						<!-- End of Menu -->
+					</div>
+					<div class="col-md-6 col-sm-6 col-xs-7">
+						
+						<nav class="sidr js-menu-side-content">
+							<ul class="">
+								<li>
+									<a href="index.html">Home
+										<span class="menu-label">NEW</span>
+									</a>
+									<ul>
+										<li>
+											<a href="#">One Page Docs</a>
+											<ul>
+												<li>
+													<a href="home-one-page.html">Static Header</a>
+												</li>
+												<li>
+													<a href="home-one-page-header-fixed.html">Sticky Header</a>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="#">Menu</a>
+											<ul>
+												<li>
+													<a href="index-2.html">Left - Off Canvas</a>
+												</li>
+												<li>
+													<a href="index-3.html">Right - Off Canvas</a>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="#">Static</a>
+											<ul>
+												<li>
+													<a href="home-subscribe.html">Subscribe</a>
+												</li>
+												<li>
+													<a href="home-web.html">Web App</a>
+												</li>
+												<li>
+													<a href="home-mobile-app.html">Mobile App</a>
+												</li>
+												<li>
+													<a href="home-soft.html">Software</a>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="#">Video</a>
+											<ul>
+												<li>
+													<a href="index-5.html">Background</a>
+												</li>
+												<li>
+													<a href="home-video-trigger.html">Trigger Simple</a>
+												</li>
+												<li>
+													<a href="home-web-video-trigger.html">Trigger Web</a>
+												</li>
+												<li>
+													<a href="home-app-video-trigger.html">Trigger App</a>
+												</li>
+												<li>
+													<a href="home-soft-video-trigger.html">Trigger Soft</a>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="index-4.html">Text Rotator</a>
+										</li>
+										<li>
+											<a href="home-with-preloader.html">With Preloader</a>
+										</li>
+									</ul>
+								</li>
+								<li>
+									<a href="#">Pages</a>
+									<ul>
+										<li>
+											<a href="panels.html">Panels</a>
+										</li>
+										<li>
+											<a href="#">Blog</a>
+											<ul>
+												<li>
+													<a href="blog-grid.html">Blog Grid</a>
+												</li>
+												<li>
+													<a href="blog-list.html">Blog List</a>
+												</li>
+												<li>
+													<a href="article.html">Article</a>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="faq.html">Faq</a>
+										</li>
+										<li>
+											<a href="notification.html">Notification</a>
+										</li>
+										<li>
+											<a href="tutorial.html">Tutorial</a>
+										</li>
+										<li>
+											<a href="login.html">Login</a>
+										</li>
+										<li>
+											<a href="register.html">Register</a>
+										</li>
+										<li>
+											<a href="404.html">404 Error</a>
+										</li>
+									</ul>
+								</li>
+								<li>
+									<a href="#">Elements</a>
+									<ul>
+										<li>
+											<a href="buttons.html">Buttons</a>
+										</li>
+										<li>
+											<a href="boxes.html">Boxes</a>
+										</li>
+										<li>
+											<a href="code.html">Code</a>
+										</li>
+										<li>
+											<a href="visual-guide.html">Visual guide</a>
+										</li>
+										<li>
+											<a href="notes.html">Notes</a>
+										</li>
+										<li>
+											<a href="files.html">File tree</a>
+										</li>
+										<li>
+											<a href="media.html">Media</a>
+										</li>
+										<li>
+											<a href="book.html">Book</a>
+										</li>
+										<li>
+											<a href="steps.html">Steps & Tabs</a>
+										</li>
+										<li>
+											<a href="misc.html">Misc</a>
+										</li>
+									</ul>
+								</li>
+								<li>
+									<a href="#">Layouts</a>
+									<ul>
+										<li>
+											<a href="views.html">Views without sidebar</a>
+										</li>
+										<li>
+											<a href="views-left.html">Views left sidebar</a>
+										</li>
+										<li>
+											<a href="views-right.html">Views right sidebar</a>
+										</li>
+										<li>
+											<a href="#">Full left sidebar</a>
+											<ul>
+												<li>
+													<a href="views-left-full.html">Regular</a>
+												</li>
+												<li>
+													<a href="views-left-full-compact.html">Compact</a>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="#">Full right sidebar</a>
+											<ul>
+												<li>
+													<a href="views-right-full.html">Regular</a>
+												</li>
+												<li>
+													<a href="views-right-full-compact.html">Compact</a>
+												</li>
+											</ul>
+										</li>
+									</ul>
+								</li>
+								<li>
+									<a href="changelog.html">Changelog</a>
+								</li>
+								<li>
+									<a href="contacts.html">Contacts</a>
+								</li>
+							</ul>
+						</nav>
+						<!-- End of Menu -->
+					</div>
+				
+				</div>
+				</div>
+			</div>
+		</div>
+		
